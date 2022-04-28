523d12d918ccb19c98eaf676e4a61eab20ad3e40
Jeremy Grosser <jeremy@synack.me>
2022-04-28 13:10:03 -0700

    highlight

--- search/server.py
+++ search/server.py
@@ -1,3 +1,7 @@
+from pygments import highlight
+from pygments.lexers import AdaLexer
+from pygments.formatters import HtmlFormatter
+
 from html.parser import HTMLParser
 from io import StringIO
 import wsgiref.simple_server
@@ -9,6 +13,7 @@ import os.path
 basedir = '/home/search/alire-backup'
 db = sqlite3.connect('file:index.db?mode=ro', uri=True)
 
+lexer = AdaLexer()
 
 
 class MLStripper(HTMLParser):
@@ -32,8 +37,8 @@ def strip_tags(html):
 
 def search(query):
     seen = set()
-    head = open('head.html', 'r').read()
-    tail = open('tail.html', 'r').read()
+    head = open('html/head.html', 'r').read()
+    tail = open('html/tail.html', 'r').read()
     query = strip_tags(query.strip('"\''))
     yield head.format(query=query).encode('utf-8')
     cur = db.cursor()
@@ -54,7 +59,7 @@ def search(query):
         yield b'<div class="result">\n'
 
         path = path.lstrip(b'./')
-        link = '{crate} <a href="/source/{path}">{filename}</a>\n'.format(
+        link = '{crate} <a href="/source/{path}.html">{filename}</a>\n'.format(
                 path=path.decode('utf-8'),
                 crate=crate.decode('utf-8'),
                 filename=filename.decode('utf-8'))
@@ -75,7 +80,7 @@ def search(query):
 
 
 def index():
-    return [open('index.html', 'rb').read()]
+    return [open('html/index.html', 'rb').read()]
 
 
 def make_headers(content_type):
@@ -83,6 +88,11 @@ def make_headers(content_type):
         ('Content-Type', content_type),
         ('Cache-Control', 'public, max-age=604800')
     ]
+
+
+def allowed_extension(path):
+    ext = path.rsplit('.', 1)[1]
+    return ext in ('html', 'ads', 'adb')
 
 
 def application(environ, start_response):
@@ -104,20 +114,42 @@ def application(environ, start_response):
             try:
                 path = environ['PATH_INFO'].split('/', 2)[-1]
                 path = os.path.abspath(os.path.join(basedir, path))
-                if not path.startswith(basedir) or not (path.endswith('.ads') or path.endswith('.adb')):
-                    start_response('400 Bad Request', make_headers('text/plain;charset=utf-8'))
-                    return [b'400 Bad Request\r\n']
+                if not path.startswith(basedir) or not allowed_extension(path):
+                    start_response('404 Not Found', make_headers('text/plain;charset=utf-8'))
+                    return [b'404 Not Found\r\n']
                 else:
-                    with open(path, 'rb') as fd:
-                        start_response('200 OK', make_headers('text/plain;charset=utf-8'))
-                        return [fd.read()]
+                    if path.endswith('.html'):
+                        query = urllib.parse.parse_qs(environ['QUERY_STRING'])
+                        lineno = query.get('lineno', None)
+                        path = path.rsplit('.', 1)[0]
+                        with open(path, 'r') as fd:
+                            if lineno is not None:
+                                hl_lines = [int(lineno[0])]
+                            else:
+                                hl_lines = []
+                            formatter = HtmlFormatter(linenos=True, cssclass="source", hl_lines=hl_lines)
+                            html = highlight(fd.read(), lexer, formatter)
+                                
+                            template = open('html/highlight.html', 'r').read()
+                            filename = path[len(basedir):].split('/', 2)[-1]
+                            raw = environ['PATH_INFO'].rsplit('.', 1)[0]
+                            data = template.format(filename=filename, raw=raw, code=html)
+                            start_response('200 OK', make_headers('text/html;charset=utf-8'))
+                            return [data.encode('utf-8')]
+                    else:
+                        with open(path, 'rb') as fd:
+                            start_response('200 OK', make_headers('text/plain;charset=utf-8'))
+                            return [fd.read()]
             except Exception as e:
                 print(str(e))
-                start_response('400 Bad Request', make_headers('text/plain;charset=utf-8'))
-                return [b'400 Bad Request\r\n']
+                start_response('500 Internal Server Error', make_headers('text/plain;charset=utf-8'))
+                return [b'500 Internal Server Error\r\n']
         elif environ['PATH_INFO'] == '/ada.svg':
             start_response('200 OK', make_headers('image/svg+xml'))
-            return [open('ada.svg', 'rb').read()]
+            return [open('res/ada.svg', 'rb').read()]
+        elif environ['PATH_INFO'] == '/search.css':
+            start_response('200 OK', make_headers('text/css;charset=utf-8'))
+            return [open('res/search.css', 'rb').read()]
         start_response('404 Not Found', make_headers('text/plain;charset=utf-8'))
         return [b'404 Not Found\r\n']
     else:
