5a522f7b8ad7c416afe3ede88cbed768a42507a8
Jeremy Grosser <jeremy@synack.me>
2022-09-16 00:52:34 -0700

    more verbosity

--- backup.sh
+++ backup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+set -x
+
 ALRARGS="--non-interactive --no-tty --no-color"
-ALR="alr1.2"
+ALR="alr1.3-dev"
 echo "Updating index"
 ${ALR} index --update-all
 echo "Getting package list"
-PACKAGES=$(${ALR} ${ALRARGS} search --list | cut -d' ' -f1 | grep -v 'NAME')
 
 TARGET="alire-$(date +%Y%m%d)"
 mkdir -p "${TARGET}"
 pushd "${TARGET}"
 
-for package in ${PACKAGES}; do
+${ALR} ${ALRARGS} search --list | cut -d' ' -f1 | grep -v 'NAME' >index.txt
+
+for package in $(cat index.txt); do
     echo "${pkg}"
     ${ALR} ${ALRARGS} get --only ${package}
 done
